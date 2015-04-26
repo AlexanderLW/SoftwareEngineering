d4e7311549ade81362f28da532d2252dda49dbe4
Alex <jf02319@georgiasouthern.edu>
2015-04-26 14:43:38 -0400

    Alex Changes

--- app/src/main/java/com/example/softwareengineering/softwareengineering/MainActivity.java
+++ app/src/main/java/com/example/softwareengineering/softwareengineering/MainActivity.java
@@ -5,12 +5,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.TextView;
 import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -21,8 +19,6 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         Typeface myTypeface = Typeface.createFromAsset(getAssets(), "fonts/DK Cool Crayon.ttf");
-        TextView mytextView = (TextView)findViewById(R.id.textView);
-        mytextView.setTypeface(myTypeface);
         Button mybutton = (Button)findViewById(R.id.begin);
         mybutton.setTypeface(myTypeface);
     }
